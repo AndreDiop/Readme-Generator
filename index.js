999a6a6104ad1e649be2c61366bfafc709b8e767
Andre Diop <itsjustandre@gmail.com>
2021-01-08 00:15:05 -0500

    Response logged in console
    -  I need to just write the results to a new file

--- index.js
+++ index.js
@@ -3,41 +3,44 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = inquirer.prompt([
-  {
-    type: "input",
-    message: "What is the title of your project?",
-    name: "Title",
-  },
-  {
-    type: "input",
-    message: "Give me a description of your project?",
-    name: "Description",
-  },
-  {
-    type: "input",
-    message: "What are the installation instructions?",
-    name: "Installation Instruction",
-  },
-  {
-    type: "input",
-    message: "What is the usage information",
-    name: "Usage Information",
-  },
-  {
-    type: "input",
-    message: "What are the contribution guidelines",
-    name: "Title",
-  },
-  {
-    type: "input",
-    message: "What is the title of your project?",
-    name: "Title",
-  },
-]);
-console.log(questions);
+const questions = inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is the title of your project?",
+      name: "Title",
+    },
+    {
+      type: "input",
+      message: "Give me a description of your project?",
+      name: "Description",
+    },
+    {
+      type: "input",
+      message: "What are the installation instructions?",
+      name: "Installation",
+    },
+    {
+      type: "input",
+      message: "What is the usage information?",
+      name: "Usage",
+    },
+    {
+      type: "input",
+      message: "What are the contribution guidelines?",
+      name: "Contributions",
+    },
+    {
+      type: "input",
+      message: "What are your test instructions?",
+      name: "Test",
+    },
+  ])
+  .then((response) => {
+    console.log(response);
+  });
 
-// What are the test instructions
+
 
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
