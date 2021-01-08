b88828b3b65c0edca627dcb6d9d8f7d462b27ef4
Andre Diop <itsjustandre@gmail.com>
2021-01-08 18:02:51 -0500

    Readmeception
    - reformatted response template
    - generated the readme for the readme generator via the readme generator!
    - I need to find a totem

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Readme-Generator
+
+# Console Readme Generator
+      
+## Table of Contents
+      
+[Description](#About)
+      
+[Installation](#Installation)
+      
+[Usage](#Usage)
+      
+[Test Instructions](#Test)
+      
+[Contributing](#Contributing)
+      
+[Questions](#Questions)
+      
+[Licenses](#Licenses)
+      
+# About
+This app builds an intuitive and completely dynamic readme file for your next project. No more having to remember Markdown syntax all you have to do is answer questions about your project and the code does the rest.
+## Installation
+ In your bash terminal, navigate to the folder containing the 'index.js' file. Once in that directory, return 'node index.js.
+ ## Usage
+You would want to use this app if you wanted a way to have a beautifully formatted readme file for your project with out the hassle of having to right it all out yourself. a better readme can make or break your project, so just let this script do the heavy lifting so you can focus on your project and not writing the readme.
+ ## Test Instructions
+The packages used on this script are from NPM. Full documentation can be found on www.npmjs.com. There are no unexpected behavior in the code that I know of.
+      
+      
+## Contributing
+I am always looking for ways to improve my code and become a better developer! Please do not hesitate to reach out to me for collaboration.
+      
+## Questions
+You can find me on [GitHub](https://www.github.com/AndreDiop) 
+You can reach me by email also at itsjustandre@gmail.com
+## License
+      
+[MIT](https://choosealicense.com/licenses/mit/) 
+      
+      
--- index.js
+++ index.js
@@ -1,62 +1,71 @@
-// TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+function init() {
+  const questions = inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+      },
+      {
+        type: "input",
+        message: "Give me a description of your project?",
+        name: "description",
+      },
+      {
+        type: "input",
+        message: "What are the installation instructions?",
+        name: "installation",
+      },
+      {
+        type: "input",
+        message: "What is the usage information?",
+        name: "usage",
+      },
+      {
+        type: "input",
+        message: "What are the contribution guidelines?",
+        name: "contributions",
+      },
+      {
+        type: "input",
+        message: "What are your test instructions?",
+        name: "test",
+      },
+      {
+        type: "input",
+        message: "What is your email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What's your Git Hub user name?",
+        name: "github",
+      },
+    ])
 
-// TODO: Create an array of questions for user input
-const questions = inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "What is the title of your project?",
-      name: "title",
-    },
-    {
-      type: "input",
-      message: "Give me a description of your project?",
-      name: "description",
-    },
-    {
-      type: "input",
-      message: "What are the installation instructions?",
-      name: "installation",
-    },
-    {
-      type: "input",
-      message: "What is the usage information?",
-      name: "usage",
-    },
-    {
-      type: "input",
-      message: "What are the contribution guidelines?",
-      name: "contributions",
-    },
-    {
-      type: "input",
-      message: "What are your test instructions?",
-      name: "test",
-    },
-  ])
-
-  // TODO: Create a function to write README file
-  .then((response) => {
-    console.log(response);
-    const readMeInfo = `
+    .then((response) => {
+      console.log(response);
+      const readMeInfo = `
 # ${response.title}
-
+      
 ## Table of Contents
-
+      
 [Description](#About)
-
+      
 [Installation](#Installation)
-
+      
 [Usage](#Usage)
-
+      
 [Test Instructions](#Test)
-
+      
 [Contributing](#Contributing)
-
+      
+[Questions](#Questions)
+      
 [Licenses](#Licenses)
-
+      
 # About
 ${response.description}
 ## Installation
@@ -65,29 +74,27 @@ ${response.installation}
 ${response.usage}
 ## Test Instructions
 ${response.test}
-
-
+      
+      
 ## Contributing
 ${response.contributions}
-
-
-
+      
+## Questions
+You can find me on [GitHub](https://www.github.com/${response.github}) 
+You can reach me by email also at ${response.email}
 ## License
-
-[MIT](https://choosealicense.com/licenses/mit/) 
-
-    `;
-    fs.writeFile("genReadMe.md", readMeInfo, (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("The shit worked homie");
-      }
+      
+[MIT](https://choosealicense.com/licenses/mit/)       
+      `;
+      // Create a function to write README file
+      fs.writeFile("genReadMe.md", readMeInfo, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("The shit worked homie");
+        }
+      });
     });
-  });
+}
 
-// // TODO: Create a function to initialize app
-// function init() {}
-
-// // // Function call to initialize app
-// init();
+init();
