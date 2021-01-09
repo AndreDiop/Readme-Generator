490e2603a5c5f85a5d5ed4c59c3b4a97b00c8583
Andre Diop <itsjustandre@gmail.com>
2021-01-09 13:13:01 -0500

    Refectored code
    - added license links to project, now license is added to readme
    - added init function to start the generator

--- index.js
+++ index.js
@@ -1,53 +1,71 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-function init() {
-  const questions = inquirer
-    .prompt([
-      {
-        type: "input",
-        message: "What is the title of your project?",
-        name: "title",
-      },
-      {
-        type: "input",
-        message: "Give me a description of your project?",
-        name: "description",
-      },
-      {
-        type: "input",
-        message: "What are the installation instructions?",
-        name: "installation",
-      },
-      {
-        type: "input",
-        message: "What is the usage information?",
-        name: "usage",
-      },
-      {
-        type: "input",
-        message: "What are the contribution guidelines?",
-        name: "contributions",
-      },
-      {
-        type: "input",
-        message: "What are your test instructions?",
-        name: "test",
-      },
-      {
-        type: "input",
-        message: "What is your email?",
-        name: "email",
-      },
-      {
-        type: "input",
-        message: "What's your Git Hub user name?",
-        name: "github",
-      },
-    ])
 
-    .then((response) => {
-      console.log(response);
-      const readMeInfo = `
+const questions = [
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Give me a description of your project?",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "What are the installation instructions?",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "What is the usage information?",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "What are the contribution guidelines?",
+    name: "contributions",
+  },
+  {
+    type: "input",
+    message: "What are your test instructions?",
+    name: "test",
+  },
+  {
+    type: "input",
+    message: "What is your email?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What's your Git Hub user name?",
+    name: "github",
+  },
+  {
+    type: "list",
+    message: "Which license do you require?",
+    name: "license",
+    choices: [
+      { name: "none", value: "This work is unlicensed" },
+      {
+        name: "BSD",
+        value: "[BSD](https://choosealicense.com/licenses/bsd-2-clause/)",
+      },
+      {
+        name: "GPL",
+        value: "[GPL](https://choosealicense.com/licenses/gpl-3.0/#)",
+      },
+      { name: "MIT", value: "[MIT](https://choosealicense.com/licenses/mit/)" },
+    ],
+  },
+];
+function init() {
+  console.log("Let the games begin!");
+  inquirer.prompt(questions).then((response) => {
+    console.log(response);
+
+    const readMeInfo = `
 # ${response.title}
       
 ## Table of Contents
@@ -83,18 +101,17 @@ ${response.contributions}
 You can find me on [GitHub](https://www.github.com/${response.github}) 
 You can reach me by email also at ${response.email}
 ## License
-      
-[MIT](https://choosealicense.com/licenses/mit/)       
-      `;
-      // Create a function to write README file
-      fs.writeFile("genReadMe.md", readMeInfo, (err) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("The shit worked homie");
-        }
-      });
+This project is covered under the following 
+licenses: ${response.license}
+    `;
+    // Create a function to write README file
+    fs.writeFile("genReadMe.md", readMeInfo, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("The shit worked homie");
+      }
     });
+  });
 }
-
 init();
